ef71b97943547f717716a393373fd566fd40b3d3
dsal3389 <dsal3389@gmail.com>
2020-03-30 15:38:33 +0300

    update

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,47 +1,11 @@
-$(document).ready(function(){
-    const main = $('main');
-    var lastFile = null;
+const nav = $('#main-nav');
+const classes = 'nav-active m-shd-l2 primary'
 
-    setListeners = (elements) => {
-        $.each(elements, (event) => {
-            const element = elements[event];
-            const elementEvents = $._data(element, 'events');
-
-            if(!elementEvents)
-                $(element).on('click', (event) => {
-                    let link = $(event.target);
-                    load(link.attr('link'));
-                });
-        });
-    };
-
-    clearLiseners = (elements) => {
-        elements.off();
-    };
-
-    load = (link) => {
-        const path = "html/pages/" +link +'.html';
-
-        if(path == lastFile) // do not send request again if link alread loaded
-            return;
-        lastFile = path;
-
-        clearLiseners($('main a'));
-        return $.ajax({
-            url: path, 
-            success: (res) =>  {
-                main.html($(res));
-                setListeners($('a'));
-            },
-            statusCode: {
-                404: () => load('404') // load 404.html page
-            }
-        });
-    };
-
-    setListeners($('a'));
-    load('home'); // load home on init
-});
-
-
+$(window).on('scroll', (event) => {
+    //console.log(window.scrollY);
+    if(window.scrollY > 0)
+        nav.addClass(classes);
+    else
+        nav.removeClass(classes);
+})
 
---
+++ src/frontend/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    const main = $('main');
+    var lastFile = null;
+
+    setListeners = (elements) => {
+        $.each(elements, (event) => {
+            const element = elements[event];
+            const elementEvents = $._data(element, 'events');
+
+            if(!elementEvents)
+                $(element).on('click', (event) => {
+                    let link = $(event.target);
+                    let attr = link.attr('link');
+                    
+                    if(attr != undefined)
+                        load(attr);
+                });
+        });
+    };
+
+    clearLiseners = (elements) => {
+        elements.off();
+    };
+
+    load = (link) => {
+        const path = "html/pages/" +link +'.html';
+
+        if(path == lastFile) // do not send request again if link alread loaded
+            return;
+        lastFile = path;
+
+        clearLiseners($('main a'));
+        return $.ajax({
+            url: path, 
+            success: (res) =>  {
+                main.html($(res));
+                setListeners($('a'));
+            },
+            statusCode: {
+                404: () => load('404') // load 404.html page
+            }
+        });
+    };
+
+    setListeners($('a'));
+    load('home'); // load home on init
+});
+
+
+
